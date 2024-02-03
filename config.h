1d864d03f37329461c0796b221330ee4aae06224
Yang Ming <yahngming@gmail.com>
2024-02-04 04:18:09 +0800

    Update config.h

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=18:antialias=true:autohint=true";
+static char *font = "monospace:size=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-	"Noto Emoji:pixelsize=18:antialias=true:autohint=true",
+	"Noto Emoji:pixelsize=14:antialias=true:autohint=true",
 };
 
 static int borderpx = 2;
