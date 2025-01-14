708f41c743c4d24e249699ebb4d055bde1650d46
Yang Ming <yahngming@gmail.com>
2025-01-15 04:40:48 +0800

    Update config.h

--- config.h
+++ config.h
@@ -106,7 +106,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.7, alphaUnfocused = 0.6;
+float alpha = 0.9, alphaUnfocused = 0.7;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
