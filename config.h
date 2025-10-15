ef423cbeceaa63c7767bb1f93dd83b2b2e9dfd38
Yang Ming <yahngming@gmail.com>
2025-10-15 23:05:11 +0800

    Update config.h

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=25:antialias=true:autohint=true";
+static char *font = "terminal:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 1;
 
 /*
